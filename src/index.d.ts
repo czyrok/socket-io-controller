2aab91bd87942b665e8863149314bb847a378bd1
Dylan <63360083+czyrok@users.noreply.github.com>
2022-11-03 17:36:38 +0100

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -9,3 +9,17 @@ export * from './error/parse-json.socket.error'
 export * from './model/socket-executor.model'
 
 export * from './interface/middleware.interface'
+
+declare module 'socket-io-controller' {
+    export * from './decorator/socket.decorator'
+
+    export * from './helper/using.helper'
+    export * from './helper/container.helper'
+
+    export * from './error/server.error'
+    export * from './error/parse-json.socket.error'
+
+    export * from './model/socket-executor.model'
+
+    export * from './interface/middleware.interface'
+}
