f041d3bd646659310255591101d2a0354a5b477f
Dylan <63360083+czyrok@users.noreply.github.com>
2022-11-03 18:19:04 +0100

    Testing

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,7 +5,3 @@ export * from './error/server.error';
 export * from './error/parse-json.socket.error';
 export * from './model/socket-executor.model';
 export * from './interface/middleware.interface';
-
-declare module "socket-io-controller" {
-    function SocketController(namespace?: string): Function;
-}
