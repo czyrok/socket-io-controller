dc0113357ed04efae0960ad221ffef33b85ed1dd
Dylan <63360083+czyrok@users.noreply.github.com>
2022-11-13 20:58:54 +0100

    Fixing decorator option issue

--- src/decorator/socket-controller.decorator.ts
+++ src/decorator/socket-controller.decorator.ts
@@ -9,7 +9,7 @@ import { ControllerArgInterface } from '../interface/arg/controller.arg.interfac
  *
  * @param namespace Namespace in which this controller's events will be registered
  */
-export function SocketController(options?: { namespace?: string, init: (io: Namespace) => void }): Function {
+export function SocketController(options?: { namespace?: string, init?: (io: Namespace) => void }): Function {
     return function (object: Function) {
         const metadata: ControllerArgInterface = {
             namespace: options?.namespace,
